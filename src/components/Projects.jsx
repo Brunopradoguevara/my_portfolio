6bf2a21e950000773f9c52d7d4a6d060b1bee074
Bruno Prado <103675938+Brunopradoguevara@users.noreply.github.com>
2023-09-25 17:15:13 -0500

    change projects images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
             />
             <ProjectCard 
               title='Explorador de Dimensiones de Rick and Morty' 
-              imgProject = '/registroDeUsuarios.png'
+              imgProject = '/rickAndMorty.png'
               descriptionProject='Embarcarte en una aventura interdimensional inspirada en la popular serie de televisión "Rick and Morty". Con esta aplicación web, podrás explorar las fascinantes dimensiones que Rick y Morty han visitado a lo largo de sus viajes.'
               technologies={["React","Api","Html","JavaScript","Css"]}
               linkDeploy='https://rick-and-morty-api-bruno.netlify.app/'
@@ -25,7 +25,7 @@ const Projects = () => {
             />
             <ProjectCard 
               title='ClimaYa - Datos Meteorológicos Instantáneos' 
-              imgProject = '/registroDeUsuarios.png'
+              imgProject = '/weatherApp.png'
               descriptionProject='Aplicación web que te brinda acceso instantáneo a la información meteorológica actualizada en tiempo real de cualquier ciudad del mundo. Simplemente ingresa el nombre de la ciudad que te interesa y descubre datos esenciales sobre las condiciones climáticas.'
               technologies={["React","Api","Html","JavaScript","Css"]}
               linkDeploy='https://weather-app-bruno.netlify.app/'
@@ -33,7 +33,7 @@ const Projects = () => {
             />
             <ProjectCard 
               title='Tienda academlo' 
-              imgProject = '/registroDeUsuarios.png'
+              imgProject = '/academloShop.png'
               descriptionProject=' Tieda academlo es una experiencia de compra en línea diseñada para los amantes de la moda y las tendencias. Puedes explorar una amplia gama de prendas de vestir de alta calidad y estilo, todo desde la comodidad de tu hogar.'
               technologies={["Html","JavaScript","Css"]}
               linkDeploy='https://ecommercebruno.netlify.app/'
