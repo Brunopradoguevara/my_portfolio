7a180b33ea9e2a83b995485ed140511825904ac0
Bruno Prado <103675938+Brunopradoguevara@users.noreply.github.com>
2023-09-27 18:53:41 -0500

    change gitHub image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,8 +21,8 @@ const Home = () => {
               <li className="technology__item"><img src="/CSS.png" alt="image CSS" className="technology__item_img" /></li>
               <li className="technology__item"><img src="/JavaScript.png" alt="image JavaScript" className="technology__item_img" /></li>
               <li className="technology__item"><img src="/React.png" alt="image React" className="technology__item_img" /></li>
-              <li className="technology__item"><img src="/Git.png" alt="image Git" className="technology__item_img" /></li>
               <li className="technology__item"><img src="/GitHub.png" alt="image GitHub" className="technology__item_img" /></li>
+              <li className="technology__item"><img src="/Git.png" alt="image Git" className="technology__item_img" /></li>
             </ul>
           </article>
         </div>
