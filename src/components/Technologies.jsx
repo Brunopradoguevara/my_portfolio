617b4b07d7dfb93b90b455e600fdbc24e14e3ca0
Bruno Prado <103675938+Brunopradoguevara@users.noreply.github.com>
2023-12-03 01:54:33 -0500

    change the technologies seccion

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -4,14 +4,14 @@ const Technologies = () => {
     <article className="technologies" id='technologies'>
         <h3 className="technologies__title">Tecnologías:</h3>
         <ul className="technologies__list">
-            <li className="technology__item"><img src="/HTML5.png" alt="image HTML5" className="technology__item_img" /></li>
-            <li className="technology__item"><img src="/CSS.png" alt="image CSS" className="technology__item_img" /></li>
-            <li className="technology__item"><img src="/Tailwind.png" alt="image Tailwind" className="technology__item_img" /></li>
-            <li className="technology__item"><img src="/JavaScript.png" alt="image JavaScript" className="technology__item_img" /></li>
-            <li className="technology__item"><img src="/Typescript.png" alt="image TypeScript" className="technology__item_img" /></li>
-            <li className="technology__item"><img src="/React.png" alt="image React" className="technology__item_img" /></li>
-            <li className="technology__item"><img src="/GitHub.png" alt="image GitHub" className="technology__item_img" /></li>
-            <li className="technology__item"><img src="/Git.png" alt="image Git" className="technology__item_img" /></li>
+            <li className="technology__item"><img src="/HTML5.png" alt="image HTML5" className="technology__item_img" /> <h4 className='technology__item_title'>HTML</h4></li>
+            <li className="technology__item"><img src="/CSS.png" alt="image CSS" className="technology__item_img" /> <h4 className='technology__item_title'>CSS</h4>   </li>
+            <li className="technology__item"><img src="/Tailwind.png" alt="image Tailwind" className="technology__item_img" /> <h4 className='technology__item_title'>Tailwind</h4> </li>
+            <li className="technology__item"><img src="/JavaScript.png" alt="image JavaScript" className="technology__item_img" /> <h4 className='technology__item_title'>JavaScript</h4> </li>
+            <li className="technology__item"><img src="/Typescript.png" alt="image TypeScript" className="technology__item_img" /> <h4 className='technology__item_title'>TypeScript</h4> </li>
+            <li className="technology__item"><img src="/React.png" alt="image React" className="technology__item_img" /> <h4 className='technology__item_title'>React</h4> </li>
+            <li className="technology__item"><img src="/GitHub.png" alt="image GitHub" className="technology__item_img" /> <h4 className='technology__item_title'>GitHub</h4> </li>
+            <li className="technology__item"><img src="/Git.png" alt="image Git" className="technology__item_img" /> <h4 className='technology__item_title'>Git</h4> </li>
         </ul>
     </article>
   )
