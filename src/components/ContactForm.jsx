dffe7f6ba74d7c152f3db92d21e51744c5cabdc2
Bruno Prado <103675938+Brunopradoguevara@users.noreply.github.com>
2023-12-04 01:47:00 -0500

    fixing contactForm send message

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,9 +5,9 @@ const ContactForm = () => {
   const refForm = useRef();
     const handleSubnit = (e) =>{
         e.preventDefault()
-        const service_ID = 'service_lb3oe6h'
+        const service_ID = 'service_ewuhj3q'
         const tamble_ID = 'template_07a4uci'
-        const ApiKey = 'NPUAfQdTTgbqYO1Q7'
+        const ApiKey = 'iWIyQpZTOSJnK-bWB'
         emailjs.sendForm(service_ID,tamble_ID,refForm.current,ApiKey)
         .then(res=>{
           console.log(res.text); 
@@ -20,12 +20,12 @@ const ContactForm = () => {
     <div id='contactForm' className="contactForm">
         <h2 className="contactForm__title">Contáctame</h2>
         <form className="contactForm__form" ref={refForm} onSubmit={handleSubnit}>
-            <label className="contactForm__label" htmlFor="">Nombre</label>
-            <input name='from_name' className="contactForm__input"  type="text" placeholder="Nombre" required/>
-            <label className="contactForm__label" htmlFor="">Correro</label>
-            <input name='from_email' className="contactForm__input" type="email" placeholder="usuario@gmail.com" required/>
-            <label  className="contactForm__label" htmlFor="">Mensaje</label>
-            <textarea name='message' className="contactForm__textarea" required cols="30" rows="10" placeholder="Deja tu mensaje aqui"></textarea>
+            <label className="contactForm__label" htmlFor="name">Nombre</label>
+            <input name='name' id='name' className="contactForm__input"  type="text" placeholder="Nombre" required/>
+            <label className="contactForm__label" htmlFor="email">Correro</label>
+            <input name='email' id='email' className="contactForm__input" type="email" placeholder="usuario@gmail.com" required/>
+            <label  className="contactForm__label" htmlFor="message">Mensaje</label>
+            <textarea name='message' id='message' className="contactForm__textarea" required cols="30" rows="10" placeholder="Deja tu mensaje aqui"></textarea>
             <button className="contactForm__btn">Enviar</button>
         </form>
     </div>
